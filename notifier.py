34484cb83ea713cd8ff990ef982d59db30d9cebb
Tedadar <sspoecker@gmx.de>
2026-06-30 07:53:14 +0200

    Refactor Telegram message sending function

--- notifier.py
+++ notifier.py
@@ -1,3 +1,7 @@
+import requests
+from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
+
+
 def send_telegram(message: str):
     print("DEBUG TELEGRAM TOKEN:", TELEGRAM_TOKEN)
     print("DEBUG TELEGRAM CHAT ID:", TELEGRAM_CHAT_ID)
@@ -5,8 +9,6 @@ def send_telegram(message: str):
     if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
         print("Telegram not configured")
         return
-
-    import requests
 
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
